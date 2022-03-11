97a47bb7d11dddbe038bc1a65fc7508c8955f6c2
= <=>
2022-03-11 13:12:09 +0300

    React template is added.

---
+++ client/src/App.js
@@ -0,0 +1,61 @@
+import React, { useEffect } from "react";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Content from "./components/Main/Content";
+import Footer from "./components/Footer/Footer";
+import { connect } from "react-redux";
+import MobileContent from "./components/MobileTablet/MobileContent";
+import getInitialData from "./actions/getInitialData";
+import { useMediaQuery } from "react-responsive";
+
+const App = ({ getInitialData, items }) => {
+  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1000px)" });
+  const loadingSpinner = () => {
+    return (
+      <div className="d-flex align-items-center justify-content-center mt-4">
+        <div class="spinner-border text-primary" role="status">
+          <span class="sr-only">Loading...</span>
+        </div>
+      </div>
+    );
+  };
+  useEffect(() => {
+    getInitialData();
+  }, []);
+  return (
+    <React.Fragment>
+      <Header />
+      {isTabletOrMobile && (
+        <React.Fragment>
+          {items === null ? (
+            loadingSpinner()
+          ) : (
+            <React.Fragment>
+              <MobileContent />
+              <Footer />
+            </React.Fragment>
+          )}
+        </React.Fragment>
+      )}
+
+      {!isTabletOrMobile && (
+        <React.Fragment>
+          {items === null ? (
+            loadingSpinner()
+          ) : (
+            <React.Fragment>
+              <Content />
+              <Footer />
+            </React.Fragment>
+          )}
+        </React.Fragment>
+      )}
+    </React.Fragment>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return { items: state.items };
+};
+
+export default connect(mapStateToProps, { getInitialData })(App);
